c5e50b5de812b6ac18a4da07a3df353225b70cbd
Fer <fernandodavidmedve56@gmail.com>
2021-10-24 19:40:08 -0300

    Puntos 3 y 4 terminados

--- src/main/java/fcad/ed/tp4/punto4/ClienteColaHeap.java
+++ src/main/java/fcad/ed/tp4/punto4/ClienteColaHeap.java
@@ -7,29 +7,28 @@ public class ClienteColaHeap {
 
     public static void main(String[] args) {
         ColaHeap<Integer> cola = new ColaHeap<>();
+
+        System.out.println("Cola: " + cola);
+
+        cola.enqueue(3);
         cola.enqueue(5);
-        NodoColaHeap<Integer> nodo1 = new NodoColaHeap(3);
-        NodoColaHeap<Integer> nodo2 = new NodoColaHeap(5);
-        NodoColaHeap<Integer> nodo3 = new NodoColaHeap(7);
-        NodoColaHeap<Integer> nodo4 = new NodoColaHeap(9);
-        NodoColaHeap<Integer> nodo5 = new NodoColaHeap(11);
+        cola.enqueue(7);
+        cola.enqueue(9);
+        cola.enqueue(11);
 
-        System.out.println("la cola esta vacia: " + cola.isEmpty());  //se espera true si esta vacia
+        System.out.println("Cola: " + cola);
 
-        cola.heap.addElement(nodo1);
-        cola.heap.addElement(nodo2);
-        cola.heap.addElement(nodo3);
-        cola.heap.addElement(nodo4);
-        cola.heap.addElement(nodo5);
+        System.out.println("isEmpty(): " + cola.isEmpty());  //se espera true si esta vacia
+        System.out.println("getFront(): " + cola.getFront());
 
-        System.out.println("---------------------------------");
-        System.out.println(cola.heap.toString());
-        System.out.println("---------------------------------");
-        System.out.println("la cola esta llena: " + cola.isFull());          //se espera true si esta llena
-        System.out.println("---------------------------------");
+        cola.dequeue();
+        System.out.println(cola);
+
+        System.out.println("isFull(): " + cola.isFull());          //se espera true si esta llena
+
         cola.makeEmpty();
-        System.out.println("---------------------------------");
-        System.out.println("se vacia la cola por lo tanto no esta llena: " + cola.isFull()); //se espera true si esta vacia
+        System.out.println("Cola: " + cola);
+        System.out.println("isEmpty(): " + cola.isEmpty()); //se espera true si esta vacia
 
     }
 }
